5583153b9ee5307e8269695cb021d892545c1a26
VladSimeonidi <simeonidiv@gmail.com>
2021-07-07 20:52:54 +0300

    created

---
+++ js/layout-layouts-empty-vue.a2619e61.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["layout-layouts-empty-vue"],{3308:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("vue-page-transition",{attrs:{name:"fade"}},[n("router-view")],1)],1)},u=[],r={name:"empty"},l=r,o=n("2877"),s=Object(o["a"])(l,a,u,!1,null,null,null);t["default"]=s.exports}}]);
+//# sourceMappingURL=layout-layouts-empty-vue.a2619e61.js.map
